5308445ac175d79100dbf7958fc3762b35cf3349
Prasanth R <97664738+Prasanth-Rc@users.noreply.github.com>
2025-03-13 18:47:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
 //complete this code
-class Animal {}
+class Animal {
+	private _species: string;
 
-class Dog extends Animal {}
+  constructor(species: string) {
+    this._species = species;
+  }
 
-class Cat extends Animal {}
+  get species(): string {
+    return this._species;
+  }
+
+  makeSound(): void {
+    console.log(`The ${this._species} makes a sound`);
+  }
+}
+
+class Dog extends Animal {
+	constructor(species: string) {
+    super(species);
+  }
+
+  purr(): void {
+    console.log("purr");
+  }
+}
+
+class Cat extends Animal {
+	  constructor(species: string) {
+    super(species);
+  }
+
+  bark(): void {
+    console.log("woof");
+  }
+}
+
+const myCat = new Cat("Siamese");
+myCat.makeSound();
+myCat.purr();
+
+// Creating an instance of Dog
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound();
+myDog.bark();  
 
 // Do not change the code below this line
 window.Animal = Animal;
